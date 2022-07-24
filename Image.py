fc5b4ec402e7271d481f45777cb60924add71cf7
MichalTrostowiecki <michal.trostowiecki@gmail.com>
2022-07-24 18:04:18 +0100

    Playing with crop and other functions from Pilow library

--- Image.py
+++ Image.py
@@ -2,5 +2,6 @@ from PIL import Image, ImageFilter
 
 img = Image.open("./pikachu.jpg")
 filtered_img = img.convert("L")
-filtered_img.save("grey.png", "png")
-print(filtered_img.mode)
+box = (100, 100, 400, 400)
+region = filtered_img.crop(box)
+region.save("grey.png", "png")
